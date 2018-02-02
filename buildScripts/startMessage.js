99d35d9b65bc96d16330bedd4786d1ac7f31e670
Yoshiaki Machi <machi@mbp.local>
2018-02-02 10:21:15 +0900

    work in progress

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,3 @@
+import chalk from 'chalk';
+
+console.log(chalk.green('Starging app in dev mode...')); // eslint-disable-line no-console
---
+++ webpack.config.dev.js
@@ -0,0 +1,26 @@
+import path from 'path';
+
+export default {
+  debug: true,
+  devtool: 'inline-source-map',
+  noInfo: false,
+  entry: [
+    path.resolve(__dirname, 'src/index')
+  ],
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'src'),
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [],
+  module: {
+    loaders: [
+      {test: /\.js$/, exclude: /node_module/, loaders: ['babel']},
+      {test: /\.css$/, loaders: ['style-loader','css-loader']}
+    ]
+  }
+}
